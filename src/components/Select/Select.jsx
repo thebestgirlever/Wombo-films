95a95df681f307ef2b695198bfa2efb86f8573e8
Mayya <maya1999@mail.ru>
2024-01-14 16:14:55 +0300

    add gh-pages

--- src/components/Select/Select.jsx
+++
@@ -1,9 +0,0 @@
-function Select(options, current) {
-  return (
-    <select name="pets" id="pet-select">
-      <option value="">--Please choose an option--</option>
-      {options.map(item => <option value={item.value}>{item.label}</option>)}
-    </select>
-  )
-
-}
